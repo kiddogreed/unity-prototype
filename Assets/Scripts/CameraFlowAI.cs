46f3f25b1f1dd5f24cc9f64f4cb81fa69646f193
kiddogreed <johnrusselledomingo@gmail.com>
2025-06-04 23:54:52 +0800

    new files

--- Assets/Scripts/CameraFlowAI.cs
+++ Assets/Scripts/CameraFlowAI.cs
@@ -5,9 +5,10 @@ public class Cameraflow : MonoBehaviour
     public Transform target;      // The object to follow
     public Vector3 offset = new(0, 5, -10); // Camera offset
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-      //this is to check if the git tracker is working
+  // Update is called once per frame
+  void LateUpdate()
+  {
+    //this is to check if the git tracker is working
+      //this is working
     }
 }
